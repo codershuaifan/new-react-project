2f86ad8aba2243129c39e52ed94ce721aabb7648
如果没有如果 <2116958551@qq.com>
2022-05-19 11:27:23 +0800

    '已完成'

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 import App from './App';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter,HashRouter } from 'react-router-dom'
 import {Provider} from 'react-redux'
 import store from './redux/store'
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById('root'))
